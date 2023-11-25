da2fc0475082a9cd61414f0616038ba4264eeaef
99kapila <shivanshu38@gmail.com>
2023-11-24 22:33:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -23,12 +22,19 @@ def index():
             'q': input_text,
             'langpair': f'{input_language}|{output_language}'
         }
-        response = requests.get(API_URL, params=params)
-        translation = response.json().get('responseData', {}).get('translatedText', '')
+
+        try:
+            response = requests.get(API_URL, params=params)
+            response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
+            translation = response.json().get('responseData', {}).get('translatedText', '')
+        except requests.RequestException as e:
+            # Log the exception for debugging
+            app.logger.exception("Error in MyMemory Translation API request:")
+            # Return a JSON response with an error message
+            return jsonify(error=f"An error occurred in translation: {str(e)}"), 500
 
     return render_template('index.html', translation=translation, input_language=input_language,
                            input_text=input_text, output_language=output_language)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
