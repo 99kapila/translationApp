400caf6470630106dcbb569c5631d8442f0aaa17
99kapila <shivanshu38@gmail.com>
2023-11-24 23:01:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -7,18 +8,36 @@ def index():
     return render_template('index.html')
 
 @app.route('/translate', methods=['POST'])
-def translate():
-    input_language = request.form.get('input_language')
-    input_text = request.form.get('input_text')
-    output_language = request.form.get('output_language')
+def index():
+    
+    input_language = ""
+    input_text = ""
+    output_language = ""
+    translation = ""
 
-    # Add your translation logic here
+    if request.method == 'POST':
+        input_language = request.form['input_language']
+        input_text = request.form['input_text']
+        output_language = request.form['output_language']
 
-    # For now, let's just echo the input text
-    output_text = input_text
+        # Make a request to the MyMemory Translation API
+        params = {
+            'q': input_text,
+            'langpair': f'{input_language}|{output_language}'
+        }
 
-    return render_template('index.html', input_language=input_language, input_text=input_text,
-                           output_language=output_language, output_text=output_text)
+        try:
+            response = requests.get(API_URL, params=params)
+            response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
+            translation = response.json().get('responseData', {}).get('translatedText', '')
+        except requests.RequestException as e:
+            # Log the complete exception for debugging
+            app.logger.exception("Error in MyMemory Translation API request:")
+            # Return a JSON response with an error message
+            return jsonify(error=f"An error occurred in translation: {str(e)}"), 500
+
+    return render_template('index.html', translation=translation, input_language=input_language,
+                           input_text=input_text, output_language=output_language)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
